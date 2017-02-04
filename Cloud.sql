eab3ae7086b71c2c906cbd1032ca306147c4e123
Alexey Presman <walkingnegative.by@gmail.com>
2017-02-05 00:18:00 +0300

    Desine

---
+++ Cloud.sql
@@ -0,0 +1,27 @@
+drop database Cloud;
+create database Cloud;
+
+use Cloud;
+
+create table Users(
+id_user int auto_increment primary key,
+email varchar(30) not null,
+pas varchar(24) not null);
+
+
+CREATE TABLE Files (
+    id_file INT AUTO_INCREMENT PRIMARY KEY,
+    file_name VARCHAR(255) NOT NULL,
+    path VARCHAR(500) NOT NULL,
+    id_user INT NOT NULL,
+    size DOUBLE NOT NULL,
+    FOREIGN KEY (id_user)
+        REFERENCES Users (id_user)
+        ON DELETE CASCADE
+);
+
+select * from Users;
+
+select * from Files;
+
+
