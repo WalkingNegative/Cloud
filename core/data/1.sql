583852f5e0b0ac3e1be74655a9aae7e72719ca2a
Alexey Presman <presman97@gmail.com>
2019-12-05 18:47:36 +0300

    Added fake db

--- core/data/1.sql
+++ core/data/1.sql
@@ -9,28 +9,35 @@ CREATE TABLE user
 
 CREATE TABLE user_token
 (
-    user_id  INT PRIMARY KEY,
-    token    VARCHAR(64) NOT NULL,
-    valid_to DATETIME    NOT NULL
+    user_token_id INT PRIMARY KEY,
+    user_id       INT         NOT NULL,
+    token         VARCHAR(64) NOT NULL,
+    valid_to      DATETIME    NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE client
 (
+    client_id  INT AUTO_INCREMENT PRIMARY KEY,
     user_id    INT         NOT NULL,
     first_name VARCHAR(50) NULL DEFAULT NULL,
     last_name  VARCHAR(50) NULL DEFAULT NULL,
     country    VARCHAR(50) NULL DEFAULT NULL,
     city       VARCHAR(50) NULL DEFAULT NULL,
-    birth      DATE        NULL DEFAULT NULL
+    birth      DATE        NULL DEFAULT NULL,
+    FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE
+
 );
 
 CREATE TABLE operator
 (
+    operator_id       INT AUTO_INCREMENT PRIMARY KEY,
     user_id           INT         NOT NULL,
     first_name        VARCHAR(50) NULL     DEFAULT NULL,
     last_name         VARCHAR(50) NULL     DEFAULT NULL,
     is_active         BOOL        NOT NULL DEFAULT TRUE,
-    registration_time DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP
+    registration_time DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE user_photo
@@ -38,7 +45,8 @@ CREATE TABLE user_photo
     photo_id  INT AUTO_INCREMENT PRIMARY KEY,
     user_id   INT      NOT NULL,
     photo_url TEXT     NOT NULL,
-    load_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    load_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE file
@@ -50,7 +58,8 @@ CREATE TABLE file
     size       VARCHAR(10)  NOT NULL,
     path       TEXT         NOT NULL,
     is_private BOOL         NOT NULL DEFAULT FALSE,
-    load_time  DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP
+    load_time  DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE action
@@ -59,12 +68,15 @@ CREATE TABLE action
     user_id     INT         NOT NULL,
     type        VARCHAR(50) NOT NULL,
     description TEXT        NULL     DEFAULT NULL,
-    action_time DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP
+    action_time DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE blocked_users
 (
+    block_id   INT AUTO_INCREMENT PRIMARY KEY,
     user_id    INT      NOT NULL,
     reason     TEXT     NULL     DEFAULT NULL,
-    block_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+    block_time DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES user (user_id) ON DELETE CASCADE
 );
